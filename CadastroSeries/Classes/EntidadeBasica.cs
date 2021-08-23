14d4f5418d65baa729946e5d16861ede04f14543
lrbertti <lrbertti@gmail.com>
2021-08-23 18:58:52 -0300

    Adicionar arquivos de projeto.

---
+++ CadastroSeries/Classes/EntidadeBasica.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CadastroSeries.Classes
+{
+    public abstract class EntidadeBasica
+    {
+        public int ID { get; protected set; }
+    }
+}
---
+++ CadastroSeries/Classes/Serie.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CadastroSeries.Classes
+{
+    public class Serie : EntidadeBasica
+    {
+        private Genero Genero { get; set; }
+        private string Titulo { get; set;}
+        private string Descricao { get; set; }
+        private int Ano { get; set; }
+        private bool Excluido { get; set; }
+
+        public Serie(int id, Genero genero, string titulo, string descricao, int ano)
+        {
+            this.ID = id;
+            this.Genero = genero;
+            this.Titulo = titulo;
+            this.Descricao = descricao;
+            this.Ano = ano;
+            this.Excluido = false;
+        }
+
+        public override string ToString()
+        {
+            string concatena_retorno = "";
+            concatena_retorno += "Genero: " + this.Genero + Environment.NewLine;
+            concatena_retorno += "Titulo: " + this.Titulo + Environment.NewLine;
+            concatena_retorno += "Descricao: " + this.Descricao + Environment.NewLine;
+            concatena_retorno += "Ano de Inicio: " + this.Ano + Environment.NewLine;
+            concatena_retorno += "Excluido: " + this.Excluido;
+            return concatena_retorno;
+        }
+
+        public string getTitulo()
+        {
+            return this.Titulo;
+        }
+
+        public int getId()
+        {
+            return this.ID;
+        }
+        public bool getExcluido()
+        {
+            return this.Excluido;
+        }
+        public void Excluir()
+        {
+            this.Excluido = true;
+        }
+    }
+}
